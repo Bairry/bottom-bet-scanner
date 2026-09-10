97c6bc45a626826184bd88f200d08a09621412ca
Bairry01 <lucas.grignon01@gmail.com>
2026-09-10 22:02:26 +0200

    Fix weekend match data: scraper/leagues.mjs

--- scraper/leagues.mjs
+++ scraper/leagues.mjs
@@ -1,3 +1,13 @@
-export const LEAGUES=[
-  ['Ligue 1','france/ligue-1'],['Premier League','england/premier-league'],['Liga','spain/laliga'],['Serie A','italy/serie-a'],['Bundesliga','germany/bundesliga'],['Ligue 2','france/ligue-2'],['Championship','england/championship'],['Liga Portugal','portugal/liga-portugal'],['Eredivisie','netherlands/eredivisie'],['Jupiler Pro League','belgium/jupiler-pro-league'],['Süper Lig','turkey/super-lig']
-].map(([name,path])=>({name,path}));
+export const LEAGUES = [
+  {name:'Premier League', sportsDb:'4328', dataCode:'E0'},
+  {name:'Championship', sportsDb:'4329', dataCode:'E1'},
+  {name:'Bundesliga', sportsDb:'4331', dataCode:'D1'},
+  {name:'Serie A', sportsDb:'4332', dataCode:'I1'},
+  {name:'Ligue 1', sportsDb:'4334', dataCode:'F1'},
+  {name:'Liga', sportsDb:'4335', dataCode:'SP1'},
+  {name:'Eredivisie', sportsDb:'4337', dataCode:'N1'},
+  {name:'Belgique', sportsDb:'4338', dataCode:'B1'},
+  {name:'Ligue 2', sportsDb:'4401', dataCode:'F2'},
+  {name:'Liga Portugal', sportsDb:null, dataCode:'P1'},
+  {name:'Süper Lig', sportsDb:null, dataCode:'T1'}
+];
