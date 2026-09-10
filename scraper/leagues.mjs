fea25673787babd2a132e44845e794379fc693e8
Bairry01 <lucas.grignon01@gmail.com>
2026-09-10 18:41:19 +0200

    Ajouter scraper/leagues.mjs

---
+++ scraper/leagues.mjs
@@ -0,0 +1,3 @@
+export const LEAGUES=[
+  ['Ligue 1','france/ligue-1'],['Premier League','england/premier-league'],['Liga','spain/laliga'],['Serie A','italy/serie-a'],['Bundesliga','germany/bundesliga'],['Ligue 2','france/ligue-2'],['Championship','england/championship'],['Liga Portugal','portugal/liga-portugal'],['Eredivisie','netherlands/eredivisie'],['Jupiler Pro League','belgium/jupiler-pro-league'],['Süper Lig','turkey/super-lig']
+].map(([name,path])=>({name,path}));
